4d2137ab0cbfaf514a4bfb7ead9e6c4df603c2d5
Coding Zeus <codingzeus1218999@gmail.com>
2023-03-11 11:19:07 +0100

    Update requirements.php

--- requirements.php
+++ requirements.php
@@ -8,7 +8,7 @@
  * In order to run this script from the web, you should copy it to the web root.
  * If you are using Linux you can create a hard link instead, using the following command:
  * ln ../requirements.php requirements.php
- */
+ */     
 
 // you may need to adjust this path to the correct Yii framework path
 // uncomment and adjust the following line if Yii is not located at the default path
