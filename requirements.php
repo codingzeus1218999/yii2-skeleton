abcc4ae200721f4e6966063501e2c4df97d02978
Coding Zeus <codingzeus1218999@gmail.com>
2023-03-11 11:19:47 +0100

    Update requirements.php

--- requirements.php
+++ requirements.php
@@ -5,7 +5,7 @@
  * In order to run this script use the following console command:
  * php requirements.php
  *
- * In order to run this script from the web, you should copy it to the web root.
+ * In order to run this script from the web, you should copy it to the web root.    
  * If you are using Linux you can create a hard link instead, using the following command:
  * ln ../requirements.php requirements.php
  */     
